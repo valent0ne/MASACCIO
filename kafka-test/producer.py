a8377601955348ac1cb0ab107a58aa54d069e2bb
Stefano <stefanovalentini94@gmail.com>
2017-10-30 23:33:16 +0100

    added kafka test and resources

---
+++ kafka-test/producer.py
@@ -0,0 +1,15 @@
+from kafka import KafkaProducer
+import time
+
+
+def main():
+    producer = KafkaProducer(bootstrap_servers='localhost:9092')
+    for i in range(40000):
+        msg = "lolo22_{}".format(i)
+        producer.send('my-topic', bytes(str(msg), 'UTF-8'))
+        print("send msg: {}".format(msg))
+    time.sleep(500)
+
+
+if __name__ == "__main__":
+    main()
