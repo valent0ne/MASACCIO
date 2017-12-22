09c7b1898a4e2deb179a2c24743bb881a61c5249
Unknown <andreaperelli.95@gmail.com>
2017-12-22 13:45:55 +0100

    Added test data
    Former-commit-id: 19daf763028bc26244a5be1aecb755566d16cb81

--- implementation/tests/kafka-test/consumer.py
+++ implementation/tests/kafka-test/consumer.py
@@ -4,12 +4,20 @@ import time
 
 def main():
     consumer = KafkaConsumer(bootstrap_servers='localhost:9092')
-    consumer.subscribe(['a1.1', 'a1.2', 'a1.3', 'a1.4', 'a1.5', 'a1.6'])
+    consumer.subscribe(['a1.7', 'a1.8', 'a1.9', 'a1.10', 'a1.11', 'a1.12'])
+    start = time.time()
+    i = 1
     while True:
         consumer.poll(100)
         for msg in consumer:
-            print("received msg: {}".format(msg.value))
-        time.sleep(0.1)
+            #print("received msg {}: {}".format(i, msg.value))
+            i = i +1
+            if i > 240000:
+                end = time.time() - start
+                print(end)
+                exit()
+
+
 
 
 if __name__ == "__main__":
