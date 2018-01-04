d4696be3822a56badb1d623fe75107af3e99b09a
Stefano Valentini <stefanovalentini94@gmail.com>
2018-01-04 11:53:24 +0100

    added async mongodb storage
    Former-commit-id: e707ec8f841e4ef33eecc74c84c7c563207772f4

--- implementation/kafkaClientStoring/src/main/java/it/univaq/masaccio/kafkaClientStoring/dao/implementation/MasaccioDaoMongoDBImpl.java
+++ implementation/kafkaClientStoring/src/main/java/it/univaq/masaccio/kafkaClientStoring/dao/implementation/MasaccioDaoMongoDBImpl.java
@@ -1,7 +1,8 @@
 package it.univaq.masaccio.kafkaClientStoring.dao.implementation;
 
 
-import com.mongodb.client.MongoCollection;
+import com.mongodb.async.SingleResultCallback;
+import com.mongodb.async.client.MongoCollection;
 import it.univaq.masaccio.kafkaClientStoring.dao.interfaces.MasaccioDaoMongoDB;
 import it.univaq.masaccio.kafkaClientStoring.dao.data.DaoDataMongoDBImpl;
 import org.bson.Document;
@@ -31,8 +32,13 @@ public class MasaccioDaoMongoDBImpl extends DaoDataMongoDBImpl implements Masacc
             LOGGER.info("inserting value to collection {}", collection);
             MongoCollection<Document> c = this.dataBase.getCollection(collection);
             Document doc = Document.parse(value);
-            c.insertOne(doc);
-            LOGGER.info("value inserted");
+            // async insertion
+            c.insertOne(doc, new SingleResultCallback<Void>() {
+                @Override
+                public void onResult(final Void result, final Throwable t) {
+                    LOGGER.info("value inserted");
+                }
+            });
         }catch (Exception e){
             LOGGER.error("cannot insert to mongo - {}", e.getMessage());
             if(LOGGER.isDebugEnabled()){
