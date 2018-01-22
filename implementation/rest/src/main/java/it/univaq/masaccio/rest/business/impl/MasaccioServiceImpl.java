e5505673e47f0665358585faf20baa338bbe3d9a
Stefano Valentini <stefanovalentini94@gmail.com>
2018-01-22 18:44:18 +0100

    added rest server
    Former-commit-id: 401d129a610145e19ca5694c48c0da8b9a49fb95

---
+++ implementation/rest/src/main/java/it/univaq/masaccio/rest/business/impl/MasaccioServiceImpl.java
@@ -0,0 +1,52 @@
+package it.univaq.masaccio.rest.business.impl;
+
+import it.univaq.masaccio.rest.business.domain.Area;
+import it.univaq.masaccio.rest.business.domain.Sensor;
+import it.univaq.masaccio.rest.business.repository.AreaRepository;
+import it.univaq.masaccio.rest.business.repository.MasaccioService;
+import it.univaq.masaccio.rest.business.repository.SensorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+@Transactional
+public class MasaccioServiceImpl implements MasaccioService{
+
+
+    @Autowired
+    private AreaRepository areaRepository;
+
+    @Autowired
+    private SensorRepository sensorRepository;
+
+
+    // Sensor services
+    public List<Sensor> findAllSensorsByArea(Long id){
+        Area area = new Area();
+        area.setId(id);
+        return sensorRepository.findAllByArea(area);
+    }
+
+    public Sensor findSensorById(Long id){
+        return sensorRepository.findOne(id);
+    }
+
+    public List<Sensor> findAllSensors(){
+        return sensorRepository.findAll();
+    }
+    // end Sensor services
+
+
+    // Area services
+    public Area findAreaById(Long id){
+        return areaRepository.findOne(id);
+    }
+
+    public List<Area> findAllAreas(){
+        return areaRepository.findAll();
+    }
+    // end Area services
+}
---
+++ implementation/rest/src/main/java/it/univaq/masaccio/rest/web/SensorController.java
@@ -0,0 +1,37 @@
+package it.univaq.masaccio.rest.web;
+
+import it.univaq.masaccio.rest.business.domain.Sensor;
+import it.univaq.masaccio.rest.business.repository.MasaccioService;
+import it.univaq.masaccio.rest.web.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/sensors")
+public class SensorController {
+
+    @Autowired
+    private MasaccioService service;
+
+    @GetMapping("/{id}")
+    public Response findSensorById(@PathVariable(value = "id") Long id) throws Exception {
+        Sensor sensor = service.findSensorById(id);
+        Response<Sensor> response = new Response<>(true, "sensor with id: " + id);
+        response.setData(sensor);
+        return response;
+    }
+
+    @GetMapping("/")
+    public Response findAllSensors() throws Exception {
+        List<Sensor> sensors = service.findAllSensors();
+        Response<List<Sensor>> response = new Response<>(true, "all sensors");
+        response.setData(sensors);
+        return response;
+    }
+
+}
